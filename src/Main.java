af5dadfa28b172a072a48730924b04a67b79811a
uliana <4lyna1510@gmail.com>
2023-04-06 01:32:23 +0200

    comit1

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int c = 40;
+        int d = 30;
+
+        int suma1 = a + b;
+        int suma2 = c + d ;
+
+
+        System.out.println( suma1 );
+        System.out.println( suma2 );
+        System.out.println( suma1 < suma2 );
+        System.out.println (suma1++);
+        System.out.println (suma2--/suma2--);
+        System.out.println (suma1 ++ < suma2--/suma2 --);
+
+
+
+    }
+}
+
