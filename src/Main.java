56defb2b8b09753cb7a081d0879293e0e9876347
uliana <4lyna1510@gmail.com>
2023-04-06 13:19:53 +0200

    vupravlena

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,29 @@
 public class Main {
 
     public static void main(String[] args) {
-        int a = 10;
-        int b = 20;
-        int c = 40;
-        int d = 30;
+        int a = 30;
+        int b = 40;
+        int c = 20;
+        int d = 10;
 
         int suma1 = a + b;
         int suma2 = c + d ;
 
 
+
         System.out.println( suma1 );
         System.out.println( suma2 );
-        System.out.println( suma1 < suma2 );
+        System.out.println( suma1 > suma2 );
+
+
         System.out.println (suma1++);
         System.out.println (suma2--/suma2--);
-        System.out.println (suma1 ++ < suma2--/suma2 --);
+        System.out.println (suma1 ++ > suma2--/suma2 --);
 
+        System.out.println (suma1%=2 );
+        System.out.println (suma2%=2 );
+
+        System.out.println (true );
 
 
     }
